720806929799ab7a25ab9d23a4bbeb55849ad165
Brett Hoyer <jbh5079@gmail.com>
2013-03-19 13:19:09 -0400

    switching back to CBB from NBA

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -43,7 +43,7 @@ require 'nokogiri'
       @picks = Pick.where(:win_or_lose => nil)
       @picks.each do |pick|
         doc= Nokogiri::HTML(open('http://www.scoresandodds.com/index.html'))
-        games = doc.css('td#contentMain .gameSection #nba+.rightShadow')
+        games = doc.css('td#contentMain .gameSection #bkc+.rightShadow')
         if games.css('.odd .finalscore').css("td[id$='#{pick.away_id}'] span").first != nil
           away_score = games.css('.odd .finalscore').css("td[id$='#{pick.away_id}'] span").first.text.to_i
           home_score = games.css('.even .finalscore').css("td[id$='#{pick.home_id}'] span").first.text.to_i
